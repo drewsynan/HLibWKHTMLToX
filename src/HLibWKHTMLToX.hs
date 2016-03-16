9dc0ab00f998f8251f116077386a75d30f80ce11
Drew Synan <dsynan@gacommunication.com>
2016-03-16 16:25:45 -0500

    fix func name

--- src/HLibWKHTMLToX.hs
+++ src/HLibWKHTMLToX.hs
@@ -4,5 +4,5 @@ import HLibWKHTMLToX.Examples
 
 main :: IO ()
 main = do
-    run_hs_example' "http://www.amazon.com" "module.jpg"
+    run_hs_example "http://www.amazon.com" "module.jpg"
     return ()
